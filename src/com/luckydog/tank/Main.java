3b8a13aeeb165621464916b15ae852e6de6902b6
ZY Wei <wzy1142552920@outlook.com>
2020-06-21 23:43:14 +0800

    将坦克坐标设定为可变值，位置可以改变

---
+++ src/com/luckydog/tank/Main.java
@@ -0,0 +1,12 @@
+package com.luckydog.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TankFrame tf = new TankFrame();
+    }
+}
---
+++ src/com/luckydog/tank/TankFrame.java
@@ -0,0 +1,32 @@
+package com.luckydog.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+    int x = 200, y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("TankWar");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("paint");
+        g.fillRect(x, y, 50, 50);
+        x += 10;
+        y += 10;
+    }
+}
