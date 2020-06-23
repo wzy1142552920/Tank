d64d45dc0a52dfccfd6a296decd66be80d25af6c
ZY Wei <wzy1142552920@outlook.com>
2020-06-24 00:29:30 +0800

    定义坦克方向enum，根据坦克方向确定坦克位移

---
+++ src/com/luckydog/tank/Dir.java
@@ -0,0 +1,5 @@
+package com.luckydog.tank;
+
+public enum Dir {
+    LEFT, UP, RIGHT, DOWN
+}
--- src/com/luckydog/tank/TankFrame.java
+++ src/com/luckydog/tank/TankFrame.java
@@ -9,6 +9,8 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     int x = 200, y = 200;
+    Dir dir = Dir.DOWN;
+    final static int SPEED = 10;
 
     public TankFrame() {
         setSize(800, 600);
@@ -29,6 +31,22 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
 //        System.out.println("paint");
         g.fillRect(x, y, 50, 50);
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            default:
+                break;
+        }
 //        x += 10;
 //        y += 10;
     }
@@ -53,6 +71,7 @@ public class TankFrame extends Frame {
                 case KeyEvent.VK_UP:
                     bU = true;
                     y -= 10;
+                    break;
                 case KeyEvent.VK_RIGHT:
                     bR = true;
                     x += 10;
@@ -64,6 +83,7 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
 
         @Override
@@ -84,6 +104,14 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
+        }
+
+        private void setMainTankDir() {
+            if (bL) dir = Dir.LEFT;
+            if (bU) dir = Dir.UP;
+            if (bR) dir = Dir.RIGHT;
+            if (bD) dir = Dir.DOWN;
         }
     }
 }
