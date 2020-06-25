5445a6d4efd2b265e4650172b3db6754c78e2090
ZY Wei <wzy1142552920@outlook.com>
2020-06-25 12:39:47 +0800

    按下ctl，坦克会发射一个子弹，必须持有frame的引用，把子弹传递给坦克

--- src/com/luckydog/tank/Tank.java
+++ src/com/luckydog/tank/Tank.java
@@ -7,6 +7,8 @@ public class Tank {
     private Dir dir = Dir.DOWN;
     private static final int SPEED = 5;
 
+    private TankFrame tf;
+
     public boolean isMoving() {
         return isMoving;
     }
@@ -25,15 +27,19 @@ public class Tank {
         this.dir = dir;
     }
 
-    public Tank(int x, int y, Dir dir) {
+    public Tank(int x, int y, Dir dir, TankFrame tf) {
         super();
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.tf = tf;
     }
 
     public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.YELLOW);
         g.fillRect(x, y, 50, 50);
+        g.setColor(c);
         move();
     }
 
@@ -55,5 +61,9 @@ public class Tank {
             default:
                 break;
         }
+    }
+
+    public void fire() {
+        tf.bullet = new Bullet(this.x, this.y, dir);
     }
 }
